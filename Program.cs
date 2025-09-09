dbbc728abae8fbba98383e5f9466a179824ab603
DESKTOP-LN7SD4G\user <user>
2025-09-09 08:41:51 -0300

    Revisao

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Pressione uma tecla para continuar...");
+Console.ReadKey();
+Thread.Sleep(1000);
+Console.WriteLine("agradeço por entrar!!!");
+Console.ResetColor();
